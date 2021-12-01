c768837f66804c2b04670205faa9181d4fdb8c24
Talha Chaudhry <talhatanveer333@gmail.com>
2021-12-01 23:40:53 +0500

    configure routes for admin and other users

--- routes/order.js
+++ routes/order.js
@@ -10,6 +10,7 @@ const {User}=require('../models/user');
 const authorization=require('../middleware/authorization');
 const isCustomer=require('../middleware/isCutomer');
 const isTailor=require('../middleware/isTailor');
+const isAdmin=require('../middleware/isAdmin');
 const sendEmail=require('../services/mailer');
 
 router.post('/', authorization, isCustomer,  async (req, res) =>{
@@ -38,24 +39,64 @@ router.post('/', authorization, isCustomer,  async (req, res) =>{
     }
     order.status='pending for approval';// by default for tailor to approve
     await order.save();
-    await sendEmail('talhatanveer333@gmail.com', 'Smart Tailor Order Confirmation', 'This is easy.');
+    //await sendEmail('talhatanveer333@gmail.com', 'Smart Tailor Order Confirmation', 'This is easy.');
     // sending back the newly created instance
     res.send(order);
 });
 
 
-router.post('/approve/:id', authorization, isTailor, async(req, res)=>{
+router.patch('/approve/:id', authorization, isTailor, async(req, res)=>{
     const orderId=req.params.id;
-    if(!orderId) return res.status(400).send('Invalid order id.');
+    if(!mongoose.Types.ObjectId.isValid(orderId)) return res.status(400).send('Invalid order id.');
 
-    const order=await Order.findOne({_id:orderId, tailor:req.user._id});
+    const order=await Order.findOne({_id:orderId/*, tailor:req.user._id*/});//uncomment this
     if(!order) return res.status(400).send('Order not found.');
 
     // else if everything is good
     order.status="Progress";
     await order.save();
-    // email sending
+    // sending email
+
+    // sending response
+    res.send(order);
     
+});
+
+router.get('/', authorization, async(req, res)=>{
+    const orders=await Order.find().or([{customer:req.user._id}, {tailor:req.user._id}]);
+    if(!orders) return res.status(404).send('No order(s) found.');
+
+    return res.send(orders);
+});
+
+router.get('/query', authorization, isAdmin, async(req, res)=>{
+    let orders;
+    const customerId=req.query.customerId;
+    const tailorId=req.query.tailorId;
+    const orderId=req.query.orderId;
+
+    if(orderId){
+        orders=await Order.find({_id:orderId});
+        if(!orders) return res.status(404).send('No order(s) found.');
+    }
+    else if(customerId && tailorId){
+        orders=await Order.find().or([{customer:customerId}, {tailor:tailorId}]);
+        if(!orders) return res.status(404).send('No order(s) found.');
+    }
+    else if(customerId){
+        orders=await Order.find({customer:customerId});
+        if(!orders) return res.status(404).send('No order(s) found.');
+    }
+    else if(tailorId){
+        orders=await Order.find({tailor:tailorId});
+        if(!orders) return res.status(404).send('No order(s) found.');
+    }
+    else{
+        orders=await Order.find().limit(15);
+        if(!orders) return res.status(404).send('No order(s) found.');
+    }
+
+    return res.send(orders);
 });
 
 module.exports=router;
